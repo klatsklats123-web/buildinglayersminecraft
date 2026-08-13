cded2c362db83bfd1bc831a2863d7ce50042c013
Erkin <klatsklats123@gmail.com>
2026-08-13 16:11:31 -0400

    Initial commit

---
+++ src/main/java/org/webtrade/untitled/Untitled.java
@@ -0,0 +1,10 @@
+package org.webtrade.untitled;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Untitled implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
